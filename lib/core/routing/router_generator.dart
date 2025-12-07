c754eb749b3182fcb5475853c3aa53ab95ac3650
Khaled Bahjat <khaledbahjat0@gmail.com>
2025-12-07 16:28:43 +0200

    router confige and some packege

---
+++ lib/core/routing/router_generator.dart
@@ -0,0 +1,16 @@
+import 'package:go_router/go_router.dart';
+import 'package:nots_app/core/routing/app_routs.dart';
+import 'package:nots_app/screens/auth/login_screen.dart';
+
+class RouterGenerator {
+  static GoRouter goRouter = GoRouter(
+    routes: [
+      GoRoute(
+        path: AppRouts.login,
+        name: AppRouts.login,
+        builder: (context, state) => LoginScreen(),
+      ),
+    ],
+    initialLocation: AppRouts.login,
+  );
+}
