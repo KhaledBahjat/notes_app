0ac568ab85cdf6652ef7d4abf8a174142661e4d6
Khaled Bahjat <khaledbahjat0@gmail.com>
2025-12-18 15:37:16 +0200

    added home screen to routs

--- lib/core/routing/app_routs.dart
+++ lib/core/routing/app_routs.dart
@@ -1,4 +1,5 @@
 class AppRouts {
   static const String login = '/login';
   static const String register = '/register';
+  static const String home = '/home';
 }
--- lib/core/routing/router_generator.dart
+++ lib/core/routing/router_generator.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:nots_app/core/routing/app_routs.dart';
 import 'package:nots_app/screens/auth/login_screen.dart';
 import 'package:nots_app/screens/auth/register_screen.dart';
+import 'package:nots_app/screens/home_screen.dart';
 
 class RouterGenerator {
   static GoRouter goRouter = GoRouter(
@@ -15,6 +16,11 @@ class RouterGenerator {
         path: AppRouts.register,
         name: AppRouts.register,
         builder: (context, state) => RegisterScreen(),
+      ),
+      GoRoute(
+        path: AppRouts.home,
+        name: AppRouts.home,
+        builder: (context, state) => HomeScreen(),
       ),
     ],
     initialLocation: AppRouts.login,
