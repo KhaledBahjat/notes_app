ef37ffe0f16af55b8428666ce4550237f75182dc
Khaled Bahjat <khaledbahjat0@gmail.com>
2025-12-18 16:17:34 +0200

    signOut method

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +8,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
+      appBar: AppBar(title: Text('Home'),actions: [
+        IconButton(
+          icon: Icon(Icons.logout),
+          onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+            GoRouter.of(context).go('/login');
+          },
+        )
+      ],),
       body: Center(child: Text('Welcome to Home Screen')),
     );
   }
